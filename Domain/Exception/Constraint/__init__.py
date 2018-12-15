d190f28f179b28ec6982e2becc64e24b1db0c0cb
MERD <merd888888@gmail.com>
2018-12-16 01:20:14 +0300

    Улучшена стабильность функции format_name

--- Domain/Exception/Constraint/__init__.py
+++ Domain/Exception/Constraint/__init__.py
@@ -10,3 +10,8 @@ class ConstraintBasenameException(ConstraintException):
 class ConstraintDictNameException(ConstraintException):
     def __init__(self):
         super().__init__('Dict must contain keys: "last_name", "first_name", [Optional: "middle_name"]')
+
+
+class ConstraintNotEmptyException(ConstraintException):
+    def __init__(self, key):
+        super().__init__(f'value from {key} must be not empty')
--- Domain/functools/Format.py
+++ Domain/functools/Format.py
@@ -3,7 +3,18 @@ from typing import Dict, List
 import pymorphy2
 
 from DataBase2 import Professor, Student, Administration, Parent
-from Domain.Exception.Constraint import ConstraintBasenameException, ConstraintDictNameException
+from Domain.Exception.Constraint import ConstraintBasenameException, ConstraintDictNameException, \
+    ConstraintNotEmptyException
+from Domain.functools.Function import None_or_empty
+
+
+class Case(object):
+    nomn = {'nomn'}
+    gent = {'gent'}
+    datv = {'datv'}
+    accs = {'accs'}
+    ablt = {'ablt'}
+    loct = {'loct'}
 
 
 def format_name(user: Dict[str, str] or Professor or Student, case=None) -> str:
@@ -25,11 +36,21 @@ def format_name(user: Dict[str, str] or Professor or Student, case=None) -> str:
                 fio = [user["last_name"], user["first_name"]]
         except KeyError:
             raise ConstraintDictNameException()
-    elif isinstance(user, (Student, Professor, Administration, Parent)):
-        if len(user.middle_name) > 0:
-            fio = [user.last_name, user.first_name, user.middle_name]
         else:
-            fio = [user.last_name, user.first_name]
+            for key, val in user.items():
+                if val in fio and None_or_empty(user[key]):
+                    raise ConstraintNotEmptyException(key)
+    elif isinstance(user, (Student, Professor, Administration, Parent)):
+        if not None_or_empty(user.last_name) and not None_or_empty(user.first_name):
+            if not None_or_empty(user.middle_name):
+                fio = [user.last_name, user.first_name, user.middle_name]
+            else:
+                fio = [user.last_name, user.first_name]
+        else:
+            if None_or_empty(user.last_name):
+                raise ConstraintNotEmptyException('last_name')
+            else:
+                raise ConstraintNotEmptyException('first_name')
     else:
         try:
             if hasattr(user, 'middle_name') and len(user.middle_name) > 0:
