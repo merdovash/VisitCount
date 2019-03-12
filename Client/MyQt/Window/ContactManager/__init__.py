1e8313750e40d50987b3f29bcb3f92b2b7d7d057
MERDovash <Oe23zk45>
2019-03-12 10:44:52 +0300

    Дополнен функционал окна настройки отчетов

--- Client/MyQt/Window/ContactManager/__init__.py
+++ Client/MyQt/Window/ContactManager/__init__.py
@@ -1,7 +1,9 @@
+import datetime
 from typing import Set, List
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QFormLayout, QLineEdit, QPushButton, QHBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QFormLayout, QLineEdit, QPushButton, QHBoxLayout, QMessageBox, \
+    QCheckBox, QTimeEdit, QSpinBox
 
 from Client.MyQt.Widgets.ComboBox.CheckComboBox import CheckableComboBox
 from Client.MyQt.Widgets.Network.BisitorRequest import BisitorRequest
@@ -28,12 +30,16 @@ class QContactManagerWindow(QWidget):
         head = QLabel('Настройки контакта')
         layout.addWidget(head, alignment=Qt.AlignHCenter, stretch=2)
 
+        # Форма
         form = QFormLayout()
+
+        # email
         email = QLineEdit()
         email.setText(user.contact.email)
 
         form.addRow(QLabel('email'), email)
 
+        # отчеты
         data_view = CheckableComboBox()
         data_view.setItems(DataView.all())
         for view in DataView.of(user):
@@ -43,6 +49,29 @@ class QContactManagerWindow(QWidget):
 
         update_views_list_btn = QPushButton('Обновить список возможных отчетов')
         form.addRow(QLabel(''), update_views_list_btn)
+
+        # активация
+        status_label = QLabel('Отправлять отчеты')
+        status_checkbox = QCheckBox()
+
+        form.addRow(status_label, status_checkbox)
+
+        # время
+        time_input = QTimeEdit()
+        form.addRow(QLabel('Время получения отчета'), time_input)
+        if user.contact.last_auto is not None:
+            time_input.setTime(user.contact.last_auto.time())
+        else:
+            time_input.setTime(datetime.time(17))
+
+        time_delta = QSpinBox()
+        time_delta.setRange(1, 5040)
+        if user.contact.interval_auto_hours is not None:
+            time_delta.setValue(user.contact.interval_auto_hours)
+        else:
+            time_delta.setValue(24)
+
+        form.addRow(QLabel('Интервал, в часах'), time_delta)
 
         layout.addLayout(form)
 
@@ -60,6 +89,17 @@ class QContactManagerWindow(QWidget):
 
             if contact.email != email.text():
                 contact.email = email.text()
+
+            if contact.auto != status_checkbox.isChecked():
+                contact.auto = status_checkbox.isChecked()
+
+            if contact.last_auto is None:
+                contact.last_auto = datetime.datetime.now()
+            if contact.last_auto.time() != time_input.time():
+                contact.last_auto -= datetime.timedelta(hours=contact.last_auto.time().hour,
+                                                        minutes=contact.last_auto.time().minute)
+                contact.last_auto += datetime.timedelta(hours=time_input.time().hour(),
+                                                        minutes=time_input.time().minute())
 
             contacts_views = ContactViews.of(user)
             current_views: List[DataView] = [cv._view for cv in contacts_views]
@@ -117,11 +157,12 @@ class QContactManagerWindow(QWidget):
                 after = DataView.all(user.session())
                 data_view.setItems(after)
 
-                new = set(after)-set(before)
+                new = set(after) - set(before)
                 QMessageBox().information(
                     self,
                     'Обновление списка отчётов',
-                    f'Загружено {len(new)} {agree_to_number("новый", len(new))} {agree_to_number("вид", len(new))} {agree_to_number("отчета", len(new))}'
+                    f'Загружено {len(new)} {agree_to_number("новый", len(new))} '
+                    f'{agree_to_number("вид", len(new))} {agree_to_number("отчета", len(new))}'
                     if len(new) else 'Уже загружены всё виды.')
 
             if check_connection():
