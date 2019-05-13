fd959831a48987dae3ed279f8dd3b85bb3a1300e
MERDovash <Oe23zk45>
2019-05-13 23:33:56 +0300

    Вынесен функционал системы в отдельный класс

--- NN/OneHot.py
+++ NN/OneHot.py
@@ -1,3 +1,13 @@
+import keras
+from keras import Input, Model
+from keras.layers import Dense
+from pandas import DataFrame
+
+from DataBase2 import Professor, Student, Discipline, Visitation, Group, Lesson, LessonType
+from Domain.Aggregation.Lessons import lessons_by_date
+from Domain.Aggregation.Visitations import visitations_rate
+
+
 class OneHot:
     def __init__(self, size, func=None):
         self.size = size
@@ -39,9 +49,125 @@ class OneHotLessonWeek(OneHot):
         return r
 
 
-class OneHotDiscipline(OneHot):
-    def __getitem__(self, lesson):
+class GroupAccOneHot(OneHot):
+    def __init__(self):
+        super().__init__(2)
+
+    def __getitem__(self, item):
         r = [0 for _ in range(self.size)]
-        r[lesson.week] = 1
+        r[round(visitations_rate(item).rate())] = 1
         return r
 
+
+class Inputer:
+    def __init__(self, obj, semester):
+        self.semester = semester
+        students = Student.of(obj)
+        self.professors = Professor.of(students, semester=semester)
+        self.disciplines = Discipline.of(students, semester=semester)
+
+        self.discipline_one_hot = OneHot(len(self.disciplines), lambda x: x.discipline)
+        self.professor_one_hot = OneHot(len(self.professors), lambda x: x.professor)
+        self.lesson_index_one_hot = OneHotLessonIndex()
+        self.lesson_day_one_hot = OneHot(6, lambda x: x.date.weekday())
+        self.lesson_week_one_hot = OneHotLessonWeek(18)
+        self.lesson_type_one_hot = OneHotLessonType(4)
+
+        self.group_acc_one_hot = GroupAccOneHot()
+
+        self.input_size = 6 + 4 + 18 + 6 + len(self.professors) + len(self.disciplines) + 1 + 2 + 6 + 1
+
+    def make(self, student):
+        visitations = set(Visitation.of(student))
+        lessons = Lesson.of(student)
+
+        X = []
+        Y = []
+
+        lesson_count_one_hot = OneHot(6, lambda x: len(lessons_by_date(student, x.date.date())))
+
+        group_rate = visitations_rate(Group.of(student, self.semester)).rate(2)
+        for index, lesson in enumerate(lessons):
+            if lesson.completed:
+                input_data = []
+                input_data.extend(self.lesson_index_one_hot[lesson])
+                input_data.extend(self.lesson_type_one_hot[lesson])
+                input_data.extend(self.lesson_week_one_hot[lesson])
+                input_data.extend(self.lesson_day_one_hot[lesson])
+                input_data.extend(self.professor_one_hot[lesson])
+                input_data.extend(self.discipline_one_hot[lesson])
+                input_data.append(group_rate)
+                input_data.extend(self.group_acc_one_hot[lessons[:index]])
+                input_data.extend(lesson_count_one_hot[lesson])
+                input_data.append(1)
+
+                output_data = [int(bool(len(set(Visitation.of(lesson)) & visitations)))]
+
+                X.append(input_data)
+                Y.append(output_data)
+        return X, Y
+
+    def description(self):
+        def param(index):
+            if index < 6:
+                return f'Занятие №{index + 1}', 'Время занятия'
+            index -= 6
+
+            if index < 4:
+                lesson_type = LessonType.get(id=index)
+                if lesson_type is None:
+                    return 'None', 'None'
+                return lesson_type.full_name(), 'Тип занятия'
+            index -= 4
+
+            if index < 18:
+                return f'{index} неделя', 'Номер недели'
+            index -= 18
+
+            if index < 6:
+                return ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб"][index], 'День недели'
+            index -= 6
+
+            if index < len(self.professors):
+                return self.professors[index].short_name(), 'Преподаватель'
+            index -= len(self.professors)
+
+            if index < len(self.disciplines):
+                return self.disciplines[index].short_name(), 'Дисциплина'
+            index -= len(self.disciplines)
+
+            if index < 3:
+                return [
+                    ('Уровень посещений', 'Уровень посещений'),
+                    ('Низкий уровень раньше', 'Уровень посещений'),
+                    ("Высокий уровень раньше", 'Уровень посещений')][index]
+            index -= 3
+
+            if index < 6:
+                return f"{index + 1} занятий сегодня", \
+                       "Кол-во занятий"
+            index -= 6
+
+            return 'Деловой подход', 'Всегда Фактор'
+
+        weights = self.model.get_weights()
+        df = DataFrame(
+            {
+                'Влияние': weight[0],
+                'Параметр': param(index)[0],
+                'Тип': param(index)[1]
+            } for index, weight in enumerate(weights[0]))
+        return df
+
+    def make_model(self):
+        input_layer = Input((self.input_size,))
+
+        output_layer = Dense(1, activation='sigmoid', kernel_initializer=keras.initializers.Zeros())(input_layer)
+
+        model = Model(inputs=input_layer, outputs=output_layer)
+
+        model.compile('adam', loss='mean_squared_error')
+
+        self.model = model
+
+        return model
--- NN/__init__.py
+++ NN/__init__.py
@@ -1,55 +1,7 @@
-from typing import Set
-
-import keras
-from keras import Model, Input, Sequential
-from keras.layers import Dense
-
 import numpy as np
-from pandas import DataFrame
 
-from DataBase2 import Student, Semester, Lesson, Professor, Discipline, Visitation, LessonType, Group
-from Domain.Aggregation.Lessons import lessons_by_date
-from Domain.Aggregation.Visitations import visitations_rate
-from NN.OneHot import OneHotLessonIndex, OneHotLessonType, OneHotLessonWeek, OneHot
-
-
-def make_train_data(lessons, student, semester):
-    visitations = set(Visitation.of(student))
-
-    X = []
-    Y = []
-
-    professor_one_hot = OneHot(len(Professor.of(lessons)), lambda x: x.professor.full_name())
-    discipline_one_hot = OneHot(len(Discipline.of(lessons)), lambda x: x.discipline.full_name())
-
-    lesson_day_one_hot = OneHot(6, lambda x: x.date.weekday())
-
-    lesson_index_one_hot = OneHotLessonIndex()
-    lesson_type_one_hot = OneHotLessonType(4)
-    lesson_week_one_hot = OneHotLessonWeek(18)
-
-    group_rate = visitations_rate(Group.of(student, semester)).rate(2)
-
-    for index, lesson in enumerate(lessons):
-        lesson_count_one_hot = OneHot(6, lambda x: len(lessons_by_date(student, lesson.date.date())))
-        if lesson.completed:
-            input_data = []
-            input_data.extend(lesson_index_one_hot[lesson])
-            input_data.extend(lesson_type_one_hot[lesson])
-            input_data.extend(lesson_week_one_hot[lesson])
-            input_data.extend(lesson_day_one_hot[lesson])
-            input_data.extend(professor_one_hot[lesson])
-            input_data.extend(discipline_one_hot[lesson])
-            input_data.append(group_rate)
-            input_data.append(visitations_rate(lessons[:index]).rate())
-            input_data.extend(lesson_count_one_hot[lesson])
-
-            output_data = [int(bool(len(set(Visitation.of(lesson)) & visitations)))]
-
-            X.append(input_data)
-            Y.append(output_data)
-
-    return X, Y
+from DataBase2 import Student, Semester, Professor
+from NN.OneHot import OneHotLessonIndex, OneHotLessonType, OneHotLessonWeek, OneHot, Inputer
 
 
 def ananlyse_student(student: Student, semester: Semester):
@@ -62,8 +14,9 @@ def ananlyse_student(student: Student, semester: Semester):
         преподаватель N входов [0 или 1],
         дисциплина M входов [0 или 1],
         общий уровень посещений в группе 1 вход [0;1],
-        уровень посещений в группе до текущего занятия [0;1],
-        количество занятий в день 6 входов [0 или 1].
+        уровень посещений в группе до текущего занятия 2 входа мало или много [0,1],
+        количество занятий в день 6 входов [0 или 1],
+        деловой подход 1 вход [1].
 
     Выход системы:
         посещений 1 выход [0 или 1]
@@ -74,85 +27,75 @@ def ananlyse_student(student: Student, semester: Semester):
     :return:
     """
 
-    def one_hot(values: Set, size: int = None):
-        if size is None:
-            size = len(values)
-        d = dict()
-        for i, item in enumerate(values):
-            p = [0 for _ in range(size)]
-            p[i] = 1
-            d[item] = p
+    data = Inputer(obj=student, semester=semester)
 
-        return d
+    model = data.make_model()
 
-    lessons = Lesson.of(student, semester=semester)
-    disciplines = Discipline.of(lessons)
-    professors = Professor.of(lessons)
-
-    input_size = 6 + 4 + 18 + 6 + len(professors) + len(disciplines) + 1 + 1 + 6
-    input_layer = Input((input_size,))
-
-    output_layer = Dense(1, activation='sigmoid', kernel_initializer=keras.initializers.Zeros())(input_layer)
-
-    model = Model(inputs=input_layer, outputs=output_layer)
-
-    model.compile('adam', loss='mean_squared_error')
-
-    X, Y = make_train_data(lessons, student, semester)
+    X, Y = data.make(student)
 
     X = np.array(X)
     Y = np.array(Y)
 
-    res = model.fit(X, Y, epochs=2500, shuffle=True, verbose=0)
-    acc = model.evaluate(X, Y)
+    acc = None
 
-    def param(index):
-        if index < 6:
-            return f'Занятие №{index + 1}', 'Время занятия'
-        if index < 10:
-            lesson_type = LessonType.get(id=index - 6)
-            if lesson_type is None:
-                return 'None', 'None'
-            return lesson_type.full_name(), 'Тип занятия'
-        if index < 28:
-            return f'{index - 10} неделя', 'Номер недели'
-        if index < 34:
-            return ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб"][34 - index-1], 'День недели'
-        if index < 34 + len(professors):
-            return professors[index - 34].short_name(), 'Преподаватель'
-        if index < 34 + len(professors) + len(disciplines):
-            return disciplines[index - 34 - len(professors)].short_name(), 'Дисциплина'
-        if index < 34 + len(professors) + len(disciplines) + 2:
-            return f'Посещения в группе {["всего", "до этого дня"][index - (len(professors) + len(disciplines) + 2)]}', \
-                   "Общая атмосфера"
-        return f"{index - (34 + len(professors) + len(disciplines) + 2)+1} занятий", "Кол-во занятий"
+    for i in range(10):
+        res = model.fit(X, Y, epochs=2500, shuffle=True, verbose=0)
+        acc = model.evaluate(X, Y)
+        if acc <= 0.05:
+            break
 
-    weights = model.get_weights()
-    df = DataFrame({
-                       'Влияние': weight[0],
-                       'Параметр': param(index)[0],
-                       'Тип': param(index)[1]
-                   } for index, weight in enumerate(weights[0]))
+    df = data.description()
+
     return df, acc
 
 
-if __name__ == '__main__':
+class MeanCounter:
+    def __init__(self):
+        self.df = None
+        self.acc = 0
+        self.count = 0
 
-    total = None
-    count = 0
-    accs = []
-    for student in Student.of(Group.get(id=3)):
-        df, acc = ananlyse_student(student, Semester.of(student))
-        accs.append(acc)
-        if total is None:
-            print(df)
-            total = df
+    def add(self, df, acc):
+        if self.df is None:
+            self.df = df
         else:
-            total['Влияние'] += df['Влияние']
-        count += 1
-        print(student, acc)
-        # print(df)
-    total['Влияние'] = total['Влияние'] / count
-    total = total.sort_values('Влияние')
-    print(max(accs), sum(accs) / len(accs), min(accs))
-    print(total)
+            self.df['Влияние'] += df['Влияние']
+
+        self.count += 1
+        self.acc += acc
+
+    def calc(self):
+        df = self.df
+        df['Влияние'] = self.df['Влияние'] / self.count
+        return df, self.acc / self.count
+
+
+def analyze(obj, semester):
+    mean = MeanCounter()
+    data = Inputer(obj, semester)
+
+    for student in Student.of(obj, semester=semester):
+
+        model = data.make_model()
+
+        X, Y = data.make(student)
+
+        X, Y = np.array(X), np.array(Y)
+
+        for i in range(10):
+            res = model.fit(X, Y, epochs=500+i*500, initial_epoch=i*500, shuffle=True, verbose=0)
+            acc = model.evaluate(X, Y)
+            if acc <= 0.05:
+                break
+
+        mean.add(data.description(), model.evaluate(X, Y))
+
+    return mean.calc()
+
+
+if __name__ == '__main__':
+    professor = Professor.get(id=1)
+    df, acc = analyze(professor, Semester.of(professor))
+
+    df.to_excel('res.xlsx')
+    print(acc)
